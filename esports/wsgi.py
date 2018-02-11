68fb7e75787231c9883754635347e663cdb6bf32
jkbm <bigman33321@hotmail.com>
2018-02-11 17:25:14 +0200

    Migrate ready HEROKU

--- esports/wsgi.py
+++ esports/wsgi.py
@@ -7,16 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 
-import os
+
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "esports.settings")
+
 
 application = get_wsgi_application()
 """
+import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "esports.settings")
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
