cce3100ce667ff6a54fe7a4561ae1784b6995ce0
jkbm <bigman33321@hotmail.com>
2018-02-11 17:10:05 +0200

    Migrate ready HEROKU

--- esports/wsgi.py
+++ esports/wsgi.py
@@ -5,7 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
+
 
 import os
 
@@ -14,6 +14,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "esports.settings")
 
 application = get_wsgi_application()
-
+"""
+from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
